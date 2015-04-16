3c0de70ebbe3f137ce3186703bab313f636239dc
Kruti <Kruti@Kruti-PC>
2015-04-16 11:47:21 -0700

    Adding Cosine and pearson similarities

---
+++ src/swm/project/findsimilarities/SimilarityOperations.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swm.project.findsimilarities;
+
+import java.util.ArrayList;
+
+
+
+/**
+ *
+ * @author Kruti
+ */
+public class SimilarityOperations {
+    public void cosineSimilarities(ArrayList<Integer> vector1, ArrayList<Integer> vector2){
+        ArrayList<Integer> dotProduct = new ArrayList<>(vector1.size());
+        for(int i = 0; i<vector1.size();i++){
+            
+        }
+    }
+}
