3112457a2e80d0ab455f1b92debcd81357923128
Kruti <Kruti@Kruti-PC>
2015-04-18 11:10:47 -0700

    Adding Cosine and pearson similarities

---
+++ test/swm/project/findsimilarities/SimilarityOperationsTest.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swm.project.findsimilarities;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kruti
+ */
+public class SimilarityOperationsTest {
+    
+    public SimilarityOperationsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of cosineSimilarities method, of class SimilarityOperations.
+     */
+    @Test
+    public void testSimilarities() {
+        System.out.println("cosineSimilarities");
+        ArrayList<Double> vector1 = new ArrayList<>();
+        ArrayList<Double> vector2 = new ArrayList<>();
+        vector1.add(1.0);
+        vector1.add(2.0);
+        vector1.add(1.0);
+        vector1.add(2.0);
+        vector1.add(5.0);
+
+        vector2.add(2.0);
+        vector2.add(4.0);
+        vector2.add(2.0);
+        vector2.add(0.0);
+        vector2.add(0.0);
+        
+
+        SimilarityOperations instance = new SimilarityOperations();
+        double expResult = 0.9948;
+        double result = instance.cosineSimilarities(vector1, vector2);
+        double p = instance.pearsonsCoefficient(vector1, vector2);
+        double n = instance.findSimilarity(vector1, vector2);
+        System.out.println(result+" , "+p+" , "+n);
+        assertEquals(expResult, result, 0.001);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of pearsonsCoefficient method, of class SimilarityOperations.
+     */
+//    @Test
+//    public void testPearsonsCoefficient() {
+//        System.out.println("pearsonsCoefficient");
+//        
+//        ArrayList<Integer> vector1 = new ArrayList<>();
+//        ArrayList<Integer> vector2 = new ArrayList<>();
+//        SimilarityOperations instance = new SimilarityOperations();
+//        double expResult = 0.0;
+//        double result = instance.pearsonsCoefficient(vector1, vector2);
+//        assertEquals(expResult, result, 0.0);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    
+}
